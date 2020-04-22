d53d0954bbb5f6049700b5c959970b568dcadfc4
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-22 14:35:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,11 +2,12 @@ def my_each(array) # put argument(s) here
   # code here
   i = 0
   while i < array.length
+      yield (puts array[i])
     i += 1
-    yield (puts array[i])
+
   end
 end
 
 tas = ['arel', 'jon', 'logan', 'spencer']
 
-my_each(tas) 
+my_each(tas)
