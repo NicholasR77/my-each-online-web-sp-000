8e630096aee8f7b5e359863762df1d263e697251
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-22 14:36:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,15 @@
 def my_each(array) # put argument(s) here
   # code here
-  i = 0
-  while i < array.length
-    yield (puts array[i])
-    i += 1
+  if block_given?
+    i = 0
+    while i < array.length
+      yield (puts array[i])
+      i += 1
+    end
+  else
+    puts "Hey! No block was given!"
   end
-end
-
+end  
 tas = ['arel', 'jon', 'logan', 'spencer']
 
 my_each(tas)
